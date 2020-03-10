7fafe0b79dfaadc34b3a4b8a302213cd630fd786
agcheb <agchebykin@gmail.com>
2020-03-10 17:37:45 +0300

    try with string message

--- src/main/kotlin/ru/tinkoff/fintech/listener/TransactionListener.kt
+++ src/main/kotlin/ru/tinkoff/fintech/listener/TransactionListener.kt
@@ -1,5 +1,6 @@
 package ru.tinkoff.fintech.listener
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 import ru.tinkoff.fintech.model.Transaction
@@ -9,7 +10,11 @@ import ru.tinkoff.fintech.service.processor.TransactionProcessor
 class TransactionListener(val processor: TransactionProcessor) {
 
     @KafkaListener(topics = ["\${spring.kafka.consumer.topic}"])
-    fun onMessage(transaction: Transaction) {
+    fun onMessage(message: String) {
+
+        println(message)
+        val objectMapper = ObjectMapper()
+        val transaction = objectMapper.readValue<Transaction>(message, Transaction::class.java)
         processor.processTransaction(transaction)
     }
 }
