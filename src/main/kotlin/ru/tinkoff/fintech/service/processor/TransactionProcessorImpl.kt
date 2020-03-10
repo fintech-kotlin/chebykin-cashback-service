f7c97551ccfd284c53ae899d20eb8f53ff2e2f97
agcheb <agchebykin@gmail.com>
2020-03-10 17:07:04 +0300

    services

--- src/main/kotlin/ru/tinkoff/fintech/service/processor/TransactionProcessorImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/processor/TransactionProcessorImpl.kt
@@ -1,10 +1,12 @@
 package ru.tinkoff.fintech.service.processor
 
+import org.springframework.stereotype.Service
 import ru.tinkoff.fintech.model.Transaction
 
+@Service
 class TransactionProcessorImpl(): TransactionProcessor {
     override fun processTransaction(transaction: Transaction) {
-        println(transaction)
+        println("loasbdsjdbkakdbaksjdbakdjbasdbk" + transaction)
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
